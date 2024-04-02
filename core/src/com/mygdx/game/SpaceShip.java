e4eee280975f4b1b4b075fae58ad7ba8ea9fc883
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-04-02 19:29:17 -0300

    add spaceship

--- core/src/com/mygdx/game/SpaceShip.java
+++ core/src/com/mygdx/game/SpaceShip.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.Sprite;;
 
 public class SpaceShip extends ApplicationAdapter {
+	//The SpriteBatch is a special class that is used to draw 2D images, like the textures we loaded.
 	SpriteBatch background;
-	Texture bgImg;
+	Texture bgImg, tShip;
+	private Sprite spaceShip;
 	
 	/**
 	 * Cria os elementos
@@ -16,6 +19,8 @@ public class SpaceShip extends ApplicationAdapter {
 	public void create () {
 		background = new SpriteBatch();
 		bgImg = new Texture("bg.png");
+		tShip = new Texture("spaceship.png");
+		spaceShip = new Sprite(tShip);
 	}
 
 	/**
@@ -26,6 +31,7 @@ public class SpaceShip extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		background.begin();
 		background.draw(bgImg, 0, 0);
+		background.draw(spaceShip, 0, 0);
 		background.end();
 	}
 	
@@ -36,5 +42,6 @@ public class SpaceShip extends ApplicationAdapter {
 	public void dispose () {
 		background.dispose();
 		bgImg.dispose();
+		tShip.dispose();
 	}
 }
