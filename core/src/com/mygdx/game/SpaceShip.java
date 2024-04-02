74e0a763c084702fe4d220f9135d4dc305046240
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-04-02 20:15:13 -0300

    add enemies

--- core/src/com/mygdx/game/SpaceShip.java
+++ core/src/com/mygdx/game/SpaceShip.java
@@ -7,17 +7,26 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+import java.util.Iterator;
 
 public class SpaceShip extends ApplicationAdapter {
 	// The SpriteBatch is a special class that is used to draw 2D images, like the
 	// textures we loaded.
 	SpriteBatch background;
-	Texture bgImg, tShip, tMissile;
+	Texture bgImg, tShip, tMissile, tEnemy;
 	private Sprite spaceShip, missile;
 	private float spaceShipPosX, spaceShipPosY, missilePosX, missilePosY;
 	private int speed = 10;
 	private boolean attack;
 	private int attackSpeed = 20;
+	private Array<Rectangle> enemies;
+	private int enemySpeed = 7;
+	private long lastEnemyTime;
+	private long enemySpawnTime = 750; // Time in ms
 
 	/**
 	 * Create elements
@@ -30,11 +39,14 @@ public class SpaceShip extends ApplicationAdapter {
 		spaceShip = new Sprite(tShip);
 		tMissile = new Texture("missile.png");
 		missile = new Sprite(tMissile);
+		tEnemy = new Texture("enemy.png");
+		enemies = new Array<Rectangle>();
 		spaceShipPosX = 0;
 		spaceShipPosY = 0;
 		missilePosX = 0;
 		missilePosY = 0;
 		attack = false;
+		lastEnemyTime = 0;
 	}
 
 	/**
@@ -44,6 +56,7 @@ public class SpaceShip extends ApplicationAdapter {
 	public void render() {
 		this.moveSpaceShip();
 		this.moveMissile();
+		this.moveEnemies();
 		ScreenUtils.clear(1, 0, 0, 1);
 		background.begin();
 		background.draw(bgImg, 0, 0);
@@ -51,6 +64,9 @@ public class SpaceShip extends ApplicationAdapter {
 			background.draw(missile, missilePosX, missilePosY);
 		}
 		background.draw(spaceShip, spaceShipPosX, spaceShipPosY);
+		for (Rectangle enemy : enemies) {
+			background.draw(tEnemy, enemy.x, enemy.y);
+		}
 		background.end();
 	}
 
@@ -105,6 +121,29 @@ public class SpaceShip extends ApplicationAdapter {
 		} else {
 			missilePosX = spaceShipPosX + (spaceShip.getWidth() / 2) - (missile.getWidth() / 2);
 			missilePosY = spaceShipPosY + (spaceShip.getHeight() / 2) - (missile.getHeight() / 2);
+		}
+	}
+
+	private void spawnEnemies() {
+		Rectangle enemy = new Rectangle(
+			Gdx.graphics.getWidth(), 
+			(MathUtils.random(0, Gdx.graphics.getHeight() - tEnemy.getHeight())), 
+			tEnemy.getWidth(), 
+			tEnemy.getHeight());
+		enemies.add(enemy);
+		lastEnemyTime = TimeUtils.millis();
+	}
+
+	private void moveEnemies() {
+		if ((TimeUtils.millis() - lastEnemyTime) > enemySpawnTime) {
+			this.spawnEnemies();
+		}
+		for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext(); ) {
+			Rectangle enemy = iter.next();
+			enemy.x -= enemySpeed;
+			if (enemy.x + tEnemy.getWidth() < 0) {
+				iter.remove();
+			}
 		}
 	}
 }
