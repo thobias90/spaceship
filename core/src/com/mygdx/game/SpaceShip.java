32c5b5843b8c3721e1e63a7a1c6fe651fa830215
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-04-02 19:33:05 -0300

    add movement to spaceship

--- core/src/com/mygdx/game/SpaceShip.java
+++ core/src/com/mygdx/game/SpaceShip.java
@@ -4,13 +4,16 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.g2d.Sprite;;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 
 public class SpaceShip extends ApplicationAdapter {
 	//The SpriteBatch is a special class that is used to draw 2D images, like the textures we loaded.
 	SpriteBatch background;
 	Texture bgImg, tShip;
 	private Sprite spaceShip;
+	private float spaceShipPositionX, spaceShipPositionY;
 	
 	/**
 	 * Cria os elementos
@@ -21,6 +24,8 @@ public class SpaceShip extends ApplicationAdapter {
 		bgImg = new Texture("bg.png");
 		tShip = new Texture("spaceship.png");
 		spaceShip = new Sprite(tShip);
+		spaceShipPositionX = 0;
+		spaceShipPositionY = 0;
 	}
 
 	/**
@@ -28,10 +33,11 @@ public class SpaceShip extends ApplicationAdapter {
 	 */
 	@Override
 	public void render () {
+		this.moveSpaceShip();
 		ScreenUtils.clear(1, 0, 0, 1);
 		background.begin();
 		background.draw(bgImg, 0, 0);
-		background.draw(spaceShip, 0, 0);
+		background.draw(spaceShip, spaceShipPositionX, spaceShipPositionY);
 		background.end();
 	}
 	
@@ -43,5 +49,20 @@ public class SpaceShip extends ApplicationAdapter {
 		background.dispose();
 		bgImg.dispose();
 		tShip.dispose();
+	}
+
+	private void moveSpaceShip() {
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			spaceShipPositionX += 10;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			spaceShipPositionX -= 10;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			spaceShipPositionY += 10;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			spaceShipPositionY -= 10;
+		}
 	}
 }
